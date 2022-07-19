242b820c0da1b449fb346d1801ddadfc3d523319
Michael <myalo12@gmail.com>
2022-07-19 12:50:00 -0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,40 @@
+const questions = [
+  {
+    id: 1,
+    title: "What is the use of a namespace in Web Development?",
+    info: "A namespace is a simple global object that is used to hold methods, properties, and other objects in them. It adds ease of use via modularity, thereby, providing users with the ability to reuse the code and avoid naming conflicts.",
+  },
+  {
+    id: 2,
+    title: "What is the use of Canvas in HTML?",
+    info: "Canvas was added onto HTML5 to give users the ability to draw graphics on the go, using JavaScript. There are a variety of methods in <canvas> to allow for the drawing of paths, circles, boxes, images, and more.",
+  },
+  {
+    id: 3,
+    title:
+      "What's the difference between a frontend programming language and a backend one?",
+    info: "Frontend languages are client-facing languages (basically: languages that build user interfaces). Backend languages are server-side languages (they speak with the database, cloud services, work with the data, and serve the data to the client).",
+  },
+  {
+    id: 4,
+    title: "What's the difference between scripting and coding?",
+    info: "Scripting is building standalone scripts that automate certain things (they're similar to Excel Macros, maybe you're familiar with these). Coding is writing code that will be part of a larger program (such as a web application or a machine learning model).",
+  },
+  {
+    id: 5,
+    title: "Is a Software Developer in Test the same as a QA Engineer?",
+    info: "No. A SDIT is fundamentally a software developer (and can actually move to a Software Developer job) who joins an application and creates programs within it to test parts of it from the inside. A QA Engineer is not a software developer, and tests things from the outside, doesn't necessarily know how to program.",
+  },
+  {
+    id: 6,
+    title:
+      "If a company needs a Javascript full stack developer, what technologies would you loook for in a candidate?",
+    info: "Probably React or Angular for the frontend, and always NodeJS for the backend. We could also talk in terms of MERN/MEAN stacks (MongoDB, Express, React/Angular, Node).",
+  },
+  {
+    id: 7,
+    title: "Can you name me some popular databases?",
+    info: "Oracle, SQL, MySQL, PostgreSQL, MongoDB.",
+  },
+];
+export default questions;
